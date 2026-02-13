f7be3c1acc4cc5d7a74143f937966fde30ff717c
Ilyas <ilyasbelhous1@gmail.com>
2026-02-13 23:20:12 +0100

    Improving debuging of movement statemachine.

--- player_debug.gd
+++ player_debug.gd
@@ -7,12 +7,16 @@ func connect_to_node(node):
 	self.player = node
 	if player is PlayerController:
 		player.rotation_statemachine.stateChange.connect(func (prev, curr) :
-			$HBoxContainer2/VBoxContainer/PrevStateValue.text = player.rotation_statemachine.get_state_identifier(prev)
-			$HBoxContainer2/VBoxContainer2/CurrentStateValue.text = player.rotation_statemachine.get_state_identifier(curr)
+			$HBoxContainer2/VBoxContainer/PrevStateValue.set_text(player.rotation_statemachine.get_state_identifier(prev))
+			$HBoxContainer2/VBoxContainer2/CurrentStateValue.set_text(player.rotation_statemachine.get_state_identifier(curr))
 			)
-
-func _process(delta: float) -> void:
-	if player is PlayerController:
-		$HBoxContainer2/VBoxContainer4/VelocityValue.text = \
-			"x: {x}, y: {y}, z: {z} ".format({"x":player.velocity.x, "y": player.velocity.y, "z":player.velocity.z})
-		$HBoxContainer2/VBoxContainer5/AccelerationValue.text = str(player.acceleration)
+		
+		player.rotation_statemachine.stateChange.connect(func(_prev, _curr) :
+			$HBoxContainer2/VBoxContainer5/SpeedValue.set_text(str(sqrt(player.velocity.dot(player.velocity)))) 
+			$HBoxContainer2/VBoxContainer4/VelocityValue.text = \
+			"x: {x}, y: {y}, z: {z} ".format(
+				{	"x": "%0.2f" % player.velocity.x ,
+					"y": "%0.2f" % player.velocity.y, 
+					"z": "%0.2f" % player.velocity.z})
+			$
+		)
