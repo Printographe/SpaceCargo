d31fcaed6eba687d516bbc962316d4d632f391f0
Ilyas <ilyasbelhous1@gmail.com>
2026-03-20 18:04:40 +0100

    Fixing bug missions stay connected to the mission panel.

--- contract_panel.gd
+++ contract_panel.gd
@@ -4,7 +4,7 @@ signal accept_mission
 signal refuse_mission
 
 
-
+var connected_mission : Mission = null;
 
 func _ready() -> void:
 	$VBoxContainer/HBoxContainer/cancelbutton.pressed.connect(self.hide)
@@ -12,6 +12,13 @@ func _ready() -> void:
 
 
 func set_contract_info(mission : Mission):
+
+	if connected_mission : 
+		$VBoxContainer/ActionButtons/ConfirmButton.disconnect("pressed", on_confirm_button_pressed)
+		$VBoxContainer/ActionButtons/RefuseButton.disconnect("pressed", on_refused_button_pressed)
+	
+	connected_mission = mission
+
 	print_rich("[color=green]Showing mission {0}[/color]".format([mission.id]))
 	
 	
@@ -27,15 +34,9 @@ func set_contract_info(mission : Mission):
 		Mission.MissionState.PENDING:
 			$VBoxContainer/ActionButtons/OngoingButton.hide()
 			
-			$VBoxContainer/ActionButtons/ConfirmButton.connect("pressed", func ():
-				self.hide()
-				mission.statemachine.switch_to(Mission.MissionState.ACCEPTED_ONGOING)
-				self.hide(), CONNECT_ONE_SHOT)
+			$VBoxContainer/ActionButtons/ConfirmButton.connect("pressed", on_confirm_button_pressed.bind(connected_mission), CONNECT_ONE_SHOT)
 				
-			$VBoxContainer/ActionButtons/RefuseButton.connect("pressed", func ():
-				self.hide()
-				mission.statemachine.switch_to(Mission.MissionState.REFUSED),
-				CONNECT_ONE_SHOT)
+			$VBoxContainer/ActionButtons/RefuseButton.connect("pressed", on_refused_button_pressed.bind(connected_mission), CONNECT_ONE_SHOT)
 	
 		Mission.MissionState.ACCEPTED_ONGOING:
 			$VBoxContainer/ActionButtons/ConfirmButton.hide()
@@ -48,3 +49,11 @@ func set_contract_info(mission : Mission):
 				.format({"id" : mission.id, "state" : mission.statemachine.get_state_identifier(mission.statemachine.current_state) }))
 	
 	self.show()
+
+func on_confirm_button_pressed(mission):
+	mission.statemachine.switch_to(Mission.MissionState.ACCEPTED_ONGOING)
+	self.hide()
+
+func on_refused_button_pressed(mission):
+	self.hide()
+	mission.statemachine.switch_to(Mission.MissionState.REFUSED)
