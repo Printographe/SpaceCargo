ea009fa3fb45a23bc5f2c3acb645da7cafc39a41
Ilyas <ilyasbelhous1@gmail.com>
2026-03-22 18:23:32 +0100

    adding life to the game

--- arriving_point.gd
+++ arriving_point.gd
@@ -4,29 +4,30 @@ extends MissionItem
 class_name ArrivingPoint
 
 
-@export var actor: PackedScene
 @export var trigger_text : String = "Hold on"
 @export var item_id : int
 
+var interaction_count = 0
+
+var sentences = ["Ju-ju-jujujujujuuuuuuul", "bientôt je me casse d'ici tu verras...", "Mon gun c'est pour quand ?"]
 
 func _ready() -> void:
 	super._ready()
-	if actor and actor.can_instantiate():
-		var actor_instance = actor.instantiate()
-		self.add_child(actor_instance)
-		$MeshInstance3D.queue_free()
-	else :
-		assert(error_string(ERR_UNCONFIGURED))
 
 
 func on_progress(body):
 	if body.carrying and body.get_carrying_id() == item_id:
 		print(trigger_text)
+		show_content("Chekov", "Cimer mon reuf")
 		taskDone.emit()
+	else: 
+		show_content("Chekov", sentences[interaction_count % len(sentences)])
+		interaction_count += 1
 
 func on_pending(player):
-	print("hello")
 	show_content("Chekov", "Hello I need my gun. Hello.", true)
+	
 
 func on_success(player):
+	show_content("Chekov", "Tié le tigre des montagnes")
 	self.queue_free()
