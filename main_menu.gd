2435bc2d2f848c910dd352960709803d498f72d3
Ilyas <ilyasbelhous1@gmail.com>
2026-07-11 19:31:54 +0200

    Refactor : Loading the scene before pushing the button to avoid screen freeze.

--- main_menu.gd
+++ main_menu.gd
@@ -1,11 +1,17 @@
 extends CanvasLayer
 
 
+var loaded_scene = preload("res://test.tscn")
+
+
 func _ready() -> void:
-	$MainMenue/DemoButton.pressed.connect(func () : 
-		get_tree().change_scene_to_file("res://test.tscn"))
-	$MainMenue/SettingsButton.pressed.connect(func() :
-		$Settings.show()
-		$MainMenue.hide()
-		)
-	$AnimationPlayer.play("main_menu")
+    $MainMenue/DemoButton.pressed.connect(load_test_scene_data.bind(loaded_scene))
+    $MainMenue/SettingsButton.pressed.connect(func() :
+        $Settings.show()
+        $MainMenue.hide()
+        )
+    $AnimationPlayer.play("main_menu")
+
+
+func load_test_scene_data(scene):
+    get_tree().change_scene_to_packed(scene)
